b11735de07d84227c8596e6ed30150ddf3f62e40
David Gonzalez Juanes <david.gonzalez.juanes@gmail.com>
2017-01-28 13:45:29 +0100

    Reorder of MainActivity

--- app/src/main/java/com/davidjuanes/popular_movies/one/activities/MainActivity.java
+++ app/src/main/java/com/davidjuanes/popular_movies/one/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.davidjuanes.popular_movies.one;
+package com.davidjuanes.popular_movies.one.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.davidjuanes.popular_movies.one.R;
 
 public class MainActivity extends AppCompatActivity {
 
